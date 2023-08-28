b6121c91f9feb365e3ea45d01d496d7a41ca9305
Teja080910 <120895053+Teja080910@users.noreply.github.com>
2023-08-28 11:56:32 +0530

    Update login.js

--- client/src/collection/cerdentials/login.js
+++ client/src/collection/cerdentials/login.js
@@ -109,7 +109,7 @@ const Login=()=>{
                                 }
                              </td>
                              <td>
-                                 <p style={{position:'absolute',margin:'12px 0px 1px 1.3%',fontSize:'15px'}}><b>{x.Num}</b></p>
+                                 <p><b>{x.Num}</b></p>
                                  <img src={"streak.png"} alt="streak" width={"55px"}></img>
                              </td>
                              </>:<b></b>
