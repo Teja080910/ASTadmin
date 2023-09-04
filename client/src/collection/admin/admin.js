4da297af2947f290291de0b589136fca0ed07aca
Ashok Remoneta <ashok.kumar@remoneta.com>
2023-09-04 22:05:14 +0530

    admin

--- client/src/collection/admin/admin.js
+++ client/src/collection/admin/admin.js
@@ -64,7 +64,7 @@ export const Admin=()=>{
                      localStorage.name=gmail;
                      localStorage.yoga='';
                      alert("Admin sucessfully logged in again");
-                     if (time <= "7:20:00 pm" && time >="5:00:00 pm")
+                     if (time != "7:20:00 pm" && time !="5:00:00 pm")
                      {
                          nav("/login")
                          window.location.reload(1);
