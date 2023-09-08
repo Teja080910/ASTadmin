d71f7cd85326fbff5364d8e58b5648370e076229
Ashok Remoneta <ashok.kumar@remoneta.com>
2023-09-09 00:56:13 +0530

    project

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Admin, Adminreg } from './collection/admin/admin';
-import Login from './collection/cerdentials/login';
-import Signup from './collection/cerdentials/signup';
-import { Home } from './collection/home/homes';
-import { Addproject, Projects } from './collection/project/project';
-import { Scrum } from './collection/scrummaster/scrum';
-import './responce.css';
-import { Yoga } from './collection/dailysadhana/dailysadhana';
-import { Pro } from './collection/project/sampleproject';
+import { BrowserRouter, Routes } from 'react-router-dom';
+// import './App.css';
+// import './responce.css';
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path="/adminlogin" element={<Admin/>}/>
-      <Route path='adminregister' element={<Adminreg/>}/>
-      <Route path="/login" element={localStorage.name===''?<Admin/>:<Login/>}/>
-      <Route path="/register" element={<Signup/>}/>
-      <Route path='/yoga' element={localStorage.yoga==="Yoga@9899"?<Yoga/>:<Home/>}/>
-      <Route path='/addproject' element={<Addproject/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-      <Route path='/scrummaster' element={<Scrum/>}/>
-      <Route path='/pro' element={<Pro/>}/>
+      <Route path='/' element={<Hom/>}/>
+      {/* <Route path="/adminlogin" element={<Admin/>}/> */}
+      {/* <Route path='adminregister' element={<Adminreg/>}/> */}
+      {/* <Route path="/login" element={localStorage.name===''?<Admin/>:<Login/>}/> */}
+      {/* <Route path="/register" element={<Signup/>}/> */}
+      {/* <Route path='/yoga' element={localStorage.yoga==="Yoga@9899"?<Yoga/>:<Home/>}/> */}
+      {/* <Route path='/addproject' element={<Addproject/>}/> */}
+      {/* <Route path='/projects' element={<Projects/>}/> */}
+      {/* <Route path='/scrummaster' element={<Scrum/>}/> */}
+      {/* <Route path='/pro' element={<Pro/>}/> */}
     </Routes>
     </BrowserRouter>
     
