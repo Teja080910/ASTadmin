f831bf5d6e0198d0ba0f7d26d9bc7eb70ca7260d
Ashok Remoneta <ashok.kumar@remoneta.com>
2023-10-05 23:20:10 +0530

    face.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Admin, Adminreg } from '../src/collection/admin/admin.js';
 import Login from '../src/collection/cerdentials/login.js';
 import Signup from '../src/collection/cerdentials/signup.js';
-import {Yoga} from '../src/collection/dailysadhana/dailysadhana.js';
-import {Addproject, Projects } from '../src/collection/project/project.js';
+import { Yoga } from '../src/collection/dailysadhana/dailysadhana.js';
+import { Addproject, Projects } from '../src/collection/project/project.js';
 import { Pro } from '../src/collection/project/sampleproject.js';
 import { Scrum } from '../src/collection/scrummaster/scrum.js';
+import { Face } from './collection/face/face.js';
 import './App.css';
 import { Home } from './collection/home/homes.js';
-import './responce.css';
 import { Send } from './collection/project/send.js';
+import './responce.css';
 function App() {
   return (
     <>
@@ -26,6 +27,7 @@ function App() {
       <Route path='/scrummaster' element={<Scrum/>}/>
       <Route path='/pro' element={<Pro/>}/>
       <Route path='/send' element={<Send/>}/>
+      <Route path='/face' element={<Face/>}/>
     </Routes>
     </BrowserRouter>
     
---
+++ client/src/collection/face/face.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as faceapi from 'face-api.js'
+export const Face=()=>
+{
+    return(
+        <>
+        
+        </>
+    )
+}
