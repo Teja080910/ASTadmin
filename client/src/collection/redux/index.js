17c5aa5376070a298a5f8248b3547b7dbca0b382
Teja <tejasimma033@gmail.com>
2024-04-15 23:23:34 +0530

    some of ui changes

---
+++ client/src/collection/redux/index.js
@@ -0,0 +1,8 @@
+// import taskReducer from "./user";
+// import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({
+//   tasks: taskReducer,
+// });
+
+// export default rootReducer;
