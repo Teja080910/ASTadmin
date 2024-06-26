c05e4df39948fe33a0678b43f1ab1660f0dc88b2
sailakshmi004 <130997937+sailakshmi004@users.noreply.github.com>
2024-06-26 20:57:21 +0530

    register login

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,8 @@ import { Appstore } from './collection/redux/login.js';
 import Sample from './collection/sample/sample.js';
 import './responce.css';
 import { HackathonSidebar } from './hackathon/hackathonsidebar/hackathonsidebar.js';
+import { RegisterForm } from './bootcamp/Register/register.js';
+import { LoginForm } from './bootcamp/login/login.js';
 function App() {
   const [set, setSet] = useState()
   const [time, setTime] = useState()
@@ -55,6 +57,10 @@ function App() {
           <Route path='/redux' element={<Appstore />} />
           <Route path='/bootcamp' element={<BootcampSidebar />} />
           <Route path='/hackathon' element={<HackathonSidebar/>}/>
+          <Route path='/bootcampregister' element={<RegisterForm/>}/>
+          <Route path='/bootcamplogin' element={<LoginForm/>}/>
+
+
 
         </Routes>
       </BrowserRouter>
