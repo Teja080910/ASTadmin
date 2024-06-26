e6e2f980b9714297e590b82cdf30755c5705b8e4
anilreddykota <22b91a5469@srkrec.ac.in>
2024-06-26 22:23:15 +0530

    added drag functionality to images on control panel

--- client/src/hackathon/timer/hackathonTimer/Controls.jsx
+++ client/src/hackathon/timer/hackathonTimer/Controls.jsx
@@ -1,8 +1,9 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { imageMappings, backgrounds } from "../../resources";
 import "./Controls.css";
 import { Select } from "@chakra-ui/react";
 import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
+
 const Controls = ({ socket }) => {
   const [leftImage, setLeftImage] = useState(imageMappings[0]?.code || "");
   const [rightImage, setRightImage] = useState(imageMappings[0]?.code || "");
@@ -22,22 +23,54 @@ const Controls = ({ socket }) => {
     }
   };
 
+  const handleDragStart = (event, code, side) => {
+    event.dataTransfer.setData("code", code);
+    event.dataTransfer.setData("side", side);
+  };
+
+  const handleDrop = (event, side) => {
+    const code = event.dataTransfer.getData("code");
+    const originalSide = event.dataTransfer.getData("side");
+
+    if (originalSide !== side) {
+      if (side === "left") {
+        setLeftImage(code);
+        socket.emit("changeSide", { code, side });
+      } else if (side === "right") {
+        setRightImage(code);
+        socket.emit("changeSide", { code, side });
+      }
+    }
+    event.preventDefault();
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="controls">
       <div className="activity-controls">
         <div className="image-mappings">
-          <h3>Activity Image </h3>
+          <h3>Activity Image</h3>
           <div className="images">
             {imageMappings?.map(
               (image) =>
                 image.path && (
-                  <div key={image.code} className="image-mapping">
+                  <div
+                    key={image.code}
+                    className="image-mapping"
+                    draggable
+                    onDragStart={(event) =>
+                      handleDragStart(event, image.code, image.side)
+                    }
+                  >
                     <img
                       src={image.path}
-                      alt={`activity  ${image.code}`}
+                      alt={`activity ${image.code}`}
                       className="mapping-img"
                     />
-                    <p>{`Code: ${image.code}`}</p>
+                    <p>{`${image.code}`}</p>
                   </div>
                 )
             )}
@@ -46,46 +79,59 @@ const Controls = ({ socket }) => {
         <div className="controls">
           <div className="control-item">
             <h3>Left Side</h3>
-            <Select
-              value={leftImage}
-              onChange={(e) => handleSelectChange(e, "left")}
-                  icon={<ArrowDropDownCircleIcon />}
-              variant='outline'
+            <div
+              className="drop-area"
+              onDrop={(event) => handleDrop(event, "left")}
+              onDragOver={handleDragOver}
             >
-              {imageMappings.map((image) => (
-                <option key={image.code} value={image.code}>
-                  {image.code}
-                </option>
-              ))}
-            </Select>
+              <Select
+                value={leftImage}
+                onChange={(e) => handleSelectChange(e, "left")}
+                icon={<ArrowDropDownCircleIcon />}
+                variant="outline"
+                   placeholder="select code or drag images"
+              >
+                {imageMappings.map((image) => (
+                  <option key={image.code} value={image.code}>
+                    {image.code}
+                  </option>
+                ))}
+              </Select>
+            </div>
           </div>
           <div className="control-item">
             <h3>Right Side</h3>
-            <Select
-              value={rightImage}
-              onChange={(e) => handleSelectChange(e, "right")}
-              icon={<ArrowDropDownCircleIcon />}
-              variant='outline'
+            <div
+              className="drop-area"
+              onDrop={(event) => handleDrop(event, "right")}
+              onDragOver={handleDragOver}
             >
-              {imageMappings.map((image) => (
-                <option key={image.code} value={image.code}>
-                  {image.code}
-                </option>
-              ))}
-            </Select>
+              <Select
+                value={rightImage}
+                onChange={(e) => handleSelectChange(e, "right")}
+                icon={<ArrowDropDownCircleIcon />}
+                variant="outline"
+                placeholder="select or drag images"
+              >
+                {imageMappings.map((image) => (
+                  <option key={image.code} value={image.code}>
+                    {image.code}
+                  </option>
+                ))}
+              </Select>
+            </div>
           </div>
         </div>
       </div>
       <div className="background-controls">
         <div className="controls">
           <div className="control-item">
-            <h3>Backgrounds </h3>
+            <h3>Backgrounds</h3>
             <Select
-              value={BackGround} 
-              variant='filled'
+              value={BackGround}
+              variant="filled"
               onChange={(e) => handleSelectChange(e, "background")}
               icon={<ArrowDropDownCircleIcon />}
-
             >
               {backgrounds.map((image) => (
                 <option key={image.code} value={image.code}>
