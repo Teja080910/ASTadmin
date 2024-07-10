443acd525be5c650dc52b26d60b1cadfb85bff74
anilreddykota <22b91a5469@srkrec.ac.in>
2024-07-10 21:37:59 +0530

    password are now protected

--- server/src/hackathon/teamregistrers/allregistrers.js
+++ server/src/hackathon/teamregistrers/allregistrers.js
@@ -1,11 +1,16 @@
-import { db1 } from "../../db.js"
-export const AllTeamRegistrers = async (res) => {
-   try {
-    const allteams = await db1.collection("Htrs").find().toArray()
+import { db1 } from "../../db.js";
+
+export const AllTeamRegistrers = async (req, res) => {
+  try {
+    const allteams = await db1
+      .collection("Htrs")
+      .find({}, { projection: { Password: 0 } }) // Exclude the password field
+      .toArray();
     if (allteams) {
-        res.json(allteams)
+      res.json(allteams);
     }
-   } catch (error) {
-    
-   }
-}
+  } catch (error) {
+    console.error("Error fetching team registrers:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
