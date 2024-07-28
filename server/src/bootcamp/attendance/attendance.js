6bbd7b93463609ebc6d5a56c2cdf5742585b34f1
anilreddykota <22b91a5469@srkrec.ac.in>
2024-07-28 12:31:41 +0530

    removed log message and fixed attendance

--- server/src/bootcamp/attendance/attendance.js
+++ server/src/bootcamp/attendance/attendance.js
@@ -7,17 +7,27 @@ export const AttendStudent = async (reg, res) => {
         const user = await db1.collection("Hackathondata").findOne({ Reg_No: reg })
         if (user?.Date !== date.toDateString()) {
             if (user?.AttendDays) {
-                day =parseInt( user?.AttendDays) + 1;
+                day = parseInt(user?.AttendDays) + 1;
             } else {
                 day = 1;
             }
-            const attend = await db1.collection("Hackathondata").findOneAndUpdate({ Reg_No: reg }, { $set: { Date: date.toDateString(), AttendDays: day,AttendTime:[...user?.AttendTime,date]} })
-            if (attend?.value) {   
-                res.json({message:"attend",data:attend})
+            const attendTime = Array.isArray(user?.AttendTime) ? [...user.AttendTime, date] : [date];
+            const attend = await db1.collection("Hackathondata").findOneAndUpdate(
+                { Reg_No: reg },
+                { $set: { Date: date.toDateString(), AttendDays: day, AttendTime: attendTime} },
+                { returnOriginal: false }
+            );
+            if (attend.value) {   
+                res.json({ message: "attend", data: attend.value })
+            } else {
+                res.json({ message: "No update was made", data: null });
             }
+        } else {
+            res.json({ message: "Attendance already marked for today", data: null });
         }
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "An error occurred", error: error.message });
     }
 }
 
@@ -28,16 +38,25 @@ export const AbsentStudent = async (reg, res) => {
         const user = await db1.collection("Hackathondata").findOne({ Reg_No: reg })
         if (user?.Date === date.toDateString()) {
             if (user?.AttendDays) {
-                day =parseInt( user?.AttendDays) - 1;
+                day = parseInt(user?.AttendDays) - 1;
             } else {
                 day = 1;
             }
-            const attend = await db1.collection("Hackathondata").findOneAndUpdate({ Reg_No: reg }, { $set: { Date:" ", AttendDays: day} })
-            if (attend?.value) {   
-                res.json({message:"absent",data:attend})
+            const attend = await db1.collection("Hackathondata").findOneAndUpdate(
+                { Reg_No: reg },
+                { $set: { Date: " ", AttendDays: day } },
+                { returnOriginal: false }
+            );
+            if (attend.value) {   
+                res.json({ message: "absent", data: attend.value })
+            } else {
+                res.json({ message: "No update was made", data: null });
             }
+        } else {
+            res.json({ message: "No attendance marked for today", data: null });
         }
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "An error occurred", error: error.message });
     }
 }
